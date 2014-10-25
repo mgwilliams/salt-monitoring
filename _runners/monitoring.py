9b8781bac29850810ef7ee0ced8191888de2bcb5
Matthew Williams <mgwilliams@gmail.com>
2014-10-25 05:23:42 +0000

    add updated pagerduty module; dispatch alerts from runner

--- _runners/monitoring.py
+++ _runners/monitoring.py
@@ -16,7 +16,10 @@ log = logging.getLogger(__name__)
 
 
 def process_checks(minion_id, data):
-
+    client = salt.client.LocalClient(__opts__['conf_file'])
+    log.info(data)
+    profiles = __opts__['monitoring']['alert_profiles']['devops']
+    log.info(profiles)
     # Update the cache
     serial = salt.payload.Serial('msgpack')
     with open(os.path.join(db, minion_id), 'w') as fn:
@@ -25,5 +28,16 @@ def process_checks(minion_id, data):
     for check, v in data.items():
         for name, results in v.items():
             if results['status']['failure'] is True:
-                # alerting goes here
-                log.warning(results)
+                tag = '{0}/{1} ({2})'.format(minion_id, check, name)
+                log.info('failure ' + tag)
+                for x in profiles:
+                    for service, info in x.items():
+                        log.info(info)
+                        tgt = info['tgt']
+                        profile = info['profile']
+                        log.info(profile)
+                        log.info(tgt)
+                        log.info(service)
+                        ret = client.cmd(tgt, '{0}.trigger'.format(service),
+                               [profile, tag, results['status']['current'],
+                                results['message'], results['info']])
